c5d4c28944a1a1cd90f4ec8b0d0b9b3c4ebe5e9b
Your Name <you@example.com>
2022-09-30 00:12:32 -0700

    Question 2 completed

--- src/arrayMethods.java
+++ src/arrayMethods.java
@@ -4,10 +4,11 @@ public class arrayMethods {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Question 1
+//Question 1=================================================================================================================================
+		
 		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
 		
-		System.out.println((ages[ages.length-1])-(ages[ages.length-ages.length]));
+		System.out.println("Last minus first number in array: " + ((ages[ages.length-1])-(ages[ages.length-ages.length])));
 
 		int[] newAges = new int[ages.length+1];
 		
@@ -20,7 +21,7 @@ public class arrayMethods {
 		
 		ages = newAges;
 		
-		System.out.println((ages[ages.length-1])-(ages[ages.length-ages.length]));
+		System.out.println("New last minus first number in array: " + ((ages[ages.length-1])-(ages[ages.length-ages.length])));
 
 		double averageSum = 0;
 		
@@ -28,8 +29,33 @@ public class arrayMethods {
 		{
 			averageSum += ages[i];
 		}
-		System.out.println(averageSum/ages.length);
+		System.out.println("Average age: " + (averageSum/ages.length));
+		
+//Question 2==================================================================================================================================
+		
+		String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+		
+	    double averageName = 0;
+		
+		for(int i=0;i<names.length;i++)
+		{
+			averageName += names[i].length();
+		}
+		System.out.println("Average letters in name length: " + (averageName/names.length));
+		
+		String fullString = "";
+		
+		for(int i=0;i<names.length;i++)
+		{
+			fullString += names[i];
+		}
+		System.out.println("Concatenated names: " + fullString);
+		
+		
+		
+		
 		
 	}
+	
 
 }
